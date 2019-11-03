2b41903685ce3a0751b0926f3a08bd72360b213f
timo.betz@me.com <timo.betz@me.com>
2019-11-03 12:05:06 +0100

    install bulma

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,42 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="http://" ></a>
+        </nav>
+    </header>
+    <section>
+        <img src="" alt="" srcset="">
+    </section>
+    <main>
+        <div>
+            <div>
+                <h1>Heading 1</h1>
+            </div>
+        </div>
+        <h2>Heading 2</h2>
+        <section>
+            <div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>
